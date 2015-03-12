f6323b4e9787978d15a9b0ffd898f3a610989002
Reza Khodadadi <rezakho@gmail.com>
2015-03-12 13:12:05 +0330

    Edited

--- src/Khayyam/Khayyam.php
+++ src/Khayyam/Khayyam.php
@@ -25,4 +25,24 @@ class Khayyam extends Carbon
 	{
 		return jDateTime::date($format, $stamp = $this->getTimestamp(), $convert = false, $jalali = true, $this->getTimezone()->getName());
 	}
+
+
+
+	public static function create($year = null, $month = null, $day = null, $hour = null, $minute = null, $second = null, $tz = null)
+    {
+        $year = ($year === null) ? jDateTime::date('Y') : $year;
+        $month = ($month === null) ? jDateTime::date('n') : $month;
+        $day = ($day === null) ? jDateTime::date('j') : $day;
+
+        if ($hour === null) {
+            $hour = jDateTime::date('G');
+            $minute = ($minute === null) ? jDateTime::date('i') : $minute;
+            $second = ($second === null) ? jDateTime::date('s') : $second;
+        } else {
+            $minute = ($minute === null) ? 0 : $minute;
+            $second = ($second === null) ? 0 : $second;
+        }
+
+        return static::createFromFormat('Y-n-j G:i:s', sprintf('%s-%s-%s %s:%02s:%02s', $year, $month, $day, $hour, $minute, $second), $tz);
+    }
 }
